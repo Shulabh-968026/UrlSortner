07ce9782dc1a731bf681d379847e140b3d97f54b
Shulabh-968026 <58615131+Shulabh-968026@users.noreply.github.com>
2021-06-04 12:40:35 +0530

    Update settings.py

--- urlsortner/settings.py
+++ urlsortner/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
